ba8901e42b68f00070595374855bab9ee6feef43
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-07-03 10:37:47 +0300

    added summary comment for ExceptionHandlerCallsWrapper

--- src/Lykke.HttpClientGenerator/Infrastructure/ExceptionHandlerCallsWrapper.cs
+++ src/Lykke.HttpClientGenerator/Infrastructure/ExceptionHandlerCallsWrapper.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Reflection;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 using Lykke.Common.Api.Contract.Responses;
 using Lykke.Common.ApiLibrary.Exceptions;
 using Refit;
 
 namespace Lykke.HttpClientGenerator.Infrastructure
 {
+    /// <summary>
+    /// Calls wrapper to handle refit ApiException and throw ClientApiException (with HttpStatusCode and ErrorResponse) instead
+    /// </summary>
+    [PublicAPI]
     public class ExceptionHandlerCallsWrapper : ICallsWrapper
     {
         public async Task<object> HandleMethodCall(MethodInfo targetMethod, object[] args, Func<Task<object>> innerHandler)
